31c9adde6ee4d969e6078a3a7fe7e4645406b8c7
jeremy.zhang <jeresuxing@163.com>
2021-08-08 17:34:46 +0800

    product completely

---
+++ product-biz/product-service/src/main/java/com/cloudalibaba/product/mybatis/mapper/ProductMapper.java
@@ -0,0 +1,42 @@
+package com.cloudalibaba.product.mybatis.mapper;
+
+import com.cloudalibaba.product.mybatis.example.ProductExample;
+import com.cloudalibaba.product.mybatis.po.Product;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface ProductMapper {
+    long countByExample(ProductExample example);
+
+    int deleteByExample(ProductExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectOneByExample(ProductExample example);
+
+    Product selectOneByExampleForUpdate(ProductExample example);
+
+    List<Product> selectByExampleWithLimit(@Param("example") ProductExample example, @Param("limit") Integer limit);
+
+    List<Product> selectByExampleForUpdate(ProductExample example);
+
+    List<Product> selectByExample(ProductExample example);
+
+    Product selectByPrimaryKeyForUpdate(Integer id);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Product record, @Param("example") ProductExample example);
+
+    int updateByExample(@Param("record") Product record, @Param("example") ProductExample example);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
