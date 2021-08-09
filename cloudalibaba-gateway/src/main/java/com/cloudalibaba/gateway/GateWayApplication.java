45754222d58885b2661a5bc16101323ca82977da
jeremy.zhang <jeresuxing@163.com>
2021-08-09 21:00:22 +0800

    gateway init

---
+++ cloudalibaba-gateway/src/main/java/com/cloudalibaba/gateway/GateWayApplication.java
@@ -0,0 +1,17 @@
+package com.cloudalibaba.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author suxing.zhang
+ * @date 2021/8/8 17:38
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
